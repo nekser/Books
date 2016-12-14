7777bd0a9bb13fb1beaea68975e6d90f82364f04
vagrant <vagrant@skeleton-zf.local>
2016-12-14 00:18:03 +0000

    code reformatted

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-require_once __DIR__.'/../module/Application/test/ApplicationTest/Bootstrap.php';
-require_once __DIR__.'/../module/Library/test/LibraryTest/Bootstrap.php';
-require_once __DIR__.'/../module/BookUser/test/BookUserTest/Bootstrap.php';
+require_once __DIR__ . '/../module/Application/test/ApplicationTest/Bootstrap.php';
+require_once __DIR__ . '/../module/Library/test/LibraryTest/Bootstrap.php';
+require_once __DIR__ . '/../module/BookUser/test/BookUserTest/Bootstrap.php';
